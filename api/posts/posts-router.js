81f126541a13297b8edb854e480614d32dd6b067
Benedict Schurwanz <19333755+benedictify@users.noreply.github.com>
2021-11-30 19:36:27 -0600

    add router for get post by id

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -4,11 +4,9 @@ const Posts = require('./posts-model')
 const router = express.Router()
 
 // url prefix for all posts routes: /api/posts
-// get all - /
+// get all: /
 router.get('/', (request, response) => {
-	console.log(request.query) // show what the request was asking for? 
-
-	Posts.find(request.query) // find by...? 
+	Posts.find()
 		.then(posts => {
 			response.status(200).json(posts);
 		})
@@ -20,6 +18,19 @@ router.get('/', (request, response) => {
 		})
 })
 
-
+// get one: /:id
+router.get('/:id', (request, response) => {
+	Posts.findById(request.params.id)
+		.then(post => {
+			// .json takes the arg and encodes it into a json object
+			post 
+				? response.status(200).json(post)
+				: response.status(404).json({ message: "The post with the specified ID does not exist" })
+		})
+		.catch(error => {
+			console.log(error);
+			response.status(500).json({message: "The post information could not be retrieved"})
+		})
+})
 
 module.exports = router
